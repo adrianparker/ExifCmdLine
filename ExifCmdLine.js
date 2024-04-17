d10818094477bb4fc12b874c0f9afc393ac0af39
Adrian Parker <adrian@iceknife.com>
2024-04-17 21:04:48 +1200

    Add -a option for all EXIF

--- ExifCmdLine.js
+++ ExifCmdLine.js
@@ -4,8 +4,11 @@ import exifr from 'exifr'
 import ExifImage from 'exif'
 import argv from 'argv'
 import { flatten } from 'flat'
-import { readFileSync } from 'fs'
-const packageJSON = JSON.parse(readFileSync('./package.json'))
+// when eslint can handle JSON module imports
+// import packageJSON from './package.json' with { type: "json" }
+const packageJSON = {
+  version: '0.0.4'
+}
 
 /**
  * Validates command line arguments and if they look OK, invokes EXIF extraction.
@@ -21,30 +24,39 @@ function start() {
     description: 'Path of image file',
     example: "'ExifCmdLine --filename=FOO.jpeg' or 'ExifCmdLine -f BAR.jpg'"
   })
+  argv.option({
+    name: 'all',
+    short: 'a',
+    type: 'boolean',
+    description: 'If all EXIF should be output, not just dates. BEFORE -f argument',
+    example: "'ExifCmdLine --all --filename=FOO.jpeg' or 'ExifCmdLine -a -f BAR.jpg'"
+  })
   const args = argv.run()
   if (Object.getOwnPropertyNames(args.options).length === 0) {
     console.log('No command line options supplied. You should supply at minimum filename via -f option.')
     argv.help()
     process.exit(1)
   }
-  if ((Object.getOwnPropertyNames(args.options).length !== 1) ||
+  if (((Object.getOwnPropertyNames(args.options).length < 1) ||
+    (Object.getOwnPropertyNames(args.options).length > 2)) ||
     (args.options.filename === null)) {
-    console.log('Please provide only filename command line option.')
+    console.log('Please provide at least filename command line option, and maximum of filename and all options.')
     argv.help()
     process.exit(2)
   }
-  processFileExif(args.options.filename)
+  processFileExif(args.options.filename, !!args.options.all)
 }
 
 /**
  * Attempts to read EXIF data from given file. If the first attemp to read using exifr does not work, tries using ExifImage.
  * If data is extracted, calls logging function.
  * @param {*} filename of image to attempt to read EXIF data from
+ * @param {boolean} whether to output all exif data or just dates
  */
-async function processFileExif(filename) {
+async function processFileExif(filename, all) {
   const exif = await exifr.parse(filename)
   if (exif) {
-    logExifDates(exif)
+    logExifDates(exif, all)
   } else {
     try {
       /* eslint-disable */
@@ -53,7 +65,7 @@ async function processFileExif(filename) {
         if (error) {
           console.log('Error: ' + error.message)
         } else {
-          logExifDates(exifData)
+          logExifDates(exifData, all)
         }
       })
     } catch (error) {
@@ -65,9 +77,11 @@ async function processFileExif(filename) {
 /**
  * Logs to console all EXIF attributes that include the word 'date'
  * @param {*} exif output from one of the exif metadata extraction modules
+ * @param {boolean} whether to output all exif data
  */
-function logExifDates(exif) {
+function logExifDates(exif, all) {
   if (exif) {
+    if (all) console.log(exif)
     // we flatten as ExifImage provides nested objects in output, whereas exifr does not.
     const flatExif = flatten(exif)
     Object.keys(flatExif)
@@ -77,6 +91,8 @@ function logExifDates(exif) {
           console.log(v, flatExif[v])
         }
       })
+  } else {
+    console.log('Error - null exif')
   }
 }
 
